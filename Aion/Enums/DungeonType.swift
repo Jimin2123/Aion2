cfb50616fe2df76a1867e0579d11e51a819536d2
Jupiter <kjimin2123@gmail.com>
2026-01-16 07:06:38 +0900

    feat(difficulty): Add Difficulty enum for game levels
    - 게임 난이도를 정의하는 Difficulty 열거형을 추가했습니다.
    - 난이도는 'none', 'normal', 'hard', 'level(Int)'로 구성되어 있습니다.
    - 각 난이도는 정렬 순서를 가지며, 이를 통해 게임 내에서 난이도를 쉽게 관리할 수 있습니다.

---
+++ Aion/Enums/DungeonType.swift
@@ -0,0 +1,12 @@
+//
+//  DungeonType.swift
+//  Aion
+//
+//  Created by 정지민 on 1/16/26.
+//
+
+enum DungeonType: String, Codable {
+    case expedition = "탐험" // 탐험 던전
+    case conquest = "정복" // 정복 던전
+    case transcendence = "초월" // 초월 던전
+}
---
+++ Aion/Models/GameInfo/DungeonConfig.swift
@@ -0,0 +1,35 @@
+//
+//  ConquestConfig.swift
+//  Aion
+//
+//  Created by 정지민 on 1/16/26.
+//
+
+import Foundation
+
+// 1. 희귀 재료
+enum RareMaterial: String {
+    case none = "없음"
+    case thought = "분노의 사념"
+    case will = "분노의 의지"
+    case ego = "분노의 자아"
+}
+
+// 2. 난이도별 세부 정보 (보통/어려움 각각의 스펙)
+struct StageDetail {
+    let id = UUID() // SwiftUI 리스트용 ID
+    let minimumCP: Int // 최소 전투력
+    let difficulty: Difficulty // 난이도
+    let kinah: Int // 보상 키나
+    let rareMaterial: RareMaterial // 드랍 재료
+    let dropRate: Double // 드랍 확률 (예: 3.0 = 3%)
+}
+
+// MARK: - 던전 구조체
+struct DungeonStage: Identifiable {
+    let id: Int // ID
+    let type: DungeonType // 던전 타입
+    let name: String // 던전 이름 (예: 크라오)
+    let stage: [StageDetail] // 던전 정보
+    let estimatedSeconds: Int = 900// 소요 시간
+}
